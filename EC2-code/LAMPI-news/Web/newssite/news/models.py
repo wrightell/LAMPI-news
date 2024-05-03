1da9c7429615e4c2be03bd0da6fe05cfe969193e
wrightell <85241100+wrightell@users.noreply.github.com>
2024-05-03 10:54:40 -0400

    Delete EC2-code/LAMPI-news/Web/newssite directory

--- EC2-code/LAMPI-news/Web/newssite/news/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.db import models
-
-class NewsItem(models.Model):
-    title = models.CharField(max_length=255)
-    url = models.URLField()
-
-    def __str__(self):
-        return self.title
--- EC2-code/LAMPI-news/Web/newssite/news/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('reading-list/', views.reading_list, name='reading_list'),
-    path('delete/<int:pk>/', views.delete_news_item, name='delete_news_item'),
-]
